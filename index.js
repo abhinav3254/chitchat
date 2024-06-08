f7f3a4544945ee11b15576fb8f4a756e5b0407e4
abhinav3254 <abhinav3254@gmail.com>
2024-06-09 00:18:53 +0530

    setting up token in cookie

--- index.js
+++ index.js
@@ -1,14 +1,44 @@
 const express = require('express');
 require('dotenv').config();
+const jwt = require('jsonwebtoken');
+const cookieParser = require("cookie-parser");
+const auth = require('./middleware/auth_middleware');
 
 const app = express();
 app.use(express.json());
+app.use(cookieParser());
 
 
-app.get('', (req, res) => {
+// authenticated route for testing purpose whether my jwt is working or not
+app.get('', auth, (req, res) => {
     return res.json({ message: 'Hello Universe!!' });
 });
 
+
+// login route
+app.get('/login', (req, res) => {
+    try {
+        let jwtSecret = process.env.JWT_SECRET;
+        let data = {
+            userId: 12
+        }
+        const token = jwt.sign(data, jwtSecret, { expiresIn: '10h' });
+        // setting token in cookie
+        res.cookie("token", token);
+        return res.status(200).json({ message: 'Logged In' });
+    } catch (error) {
+        console.log(error);
+        res.json({ message: 'something went wrong' });
+    }
+});
+
+
+
+// logout route
+app.get('/logout', auth, (req, res) => {
+    // removing cookie
+    return res.clearCookie('token').status(200).json({ message: "Successfully logged out 😏 🍀" });
+});
 
 
 const port = process.env.PORT || 3001;
---
+++ middleware/auth_middleware.js
@@ -0,0 +1,17 @@
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+
+function verifyToken(req, res, next) {
+    const token = req.cookies.token;
+    if (!token) return res.status(401).json({ message: 'Access denied' });
+    try {
+        let jwtSecret = process.env.JWT_SECRET;
+        const decoded = jwt.verify(token, jwtSecret);
+        next();
+    } catch (error) {
+        res.status(401).json({ error: 'Invalid token' });
+    }
+};
+
+module.exports = verifyToken;
