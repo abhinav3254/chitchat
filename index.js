3f01336ccd581d2ae99b008b2575c36478031d83
abhinav3254 <abhinav3254@gmail.com>
2024-06-08 23:44:33 +0530

    adding start command in package.json

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+require('dotenv').config();
+
+const app = express();
+app.use(express.json());
+
+
+
+app.get('', (req, res) => {
+    return res.json({ message: 'Hello Universe!!' });
+});
+
+
+const port = process.env.PORT || 3001;
+
+app.listen(port, () => {
+    console.log(`server up and listening on port number ${port}`);
+})
